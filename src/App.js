384b1ab37ae5c64fe88c0e85f6de95d6d4e44ee6
abraham <abrahamsuarezolay@gmail.com>
2023-03-21 00:15:01 +0000

    Added axios and useState imports

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
 
 function App() {
   return (
     <div className="App">
-     
+      <h1>Hello</h1>
     </div>
   );
 }
