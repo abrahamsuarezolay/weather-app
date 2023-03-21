2bd989c04717a3cee2ecaeedefdd7a668c5ef19f
abraham <abrahamsuarezolay@gmail.com>
2023-03-21 00:32:57 +0000

    Basic html structure and image resources added to folder

--- src/App.js
+++ src/App.js
@@ -2,9 +2,35 @@ import React, { useState } from "react";
 import axios from "axios";
 
 function App() {
+
+  //const url = "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=810b2fb3501d5e255b028d8a32f496e6"
+
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <div className="container">
+        <div className="top">
+          <div className="location">
+            <p>Glasgow</p>
+          </div>
+          <div className="temperature">
+            <p>10 Degrees</p>
+          </div>
+          <div className="description">
+            <p>Cloudy</p>
+          </div>
+        </div>
+        <div className="bottom">
+          <div className="feels">
+            <p>10 Degrees</p>
+          </div>
+          <div className="humidity">
+            <p>10 Degrees</p>
+          </div>
+          <div className="wind">
+            <p>10 Degrees</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
